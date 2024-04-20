b064b4f9c0822b98074378512fa2fb9112be5cf0
tradersquareoff <75670927+tradersquareoff@users.noreply.github.com>
2024-04-20 23:45:49 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,76 @@
-service ceremonyclient stop
+#!/bin/bash
+
+# Define a spinner function
+function spinner() {
+  local pid=$1
+  local delay=0.75
+  local spinners="/-\|/"
+
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    for ((i=0; i<${#spinners}; i++)); do
+      local spin="${spinners[$i]}"
+      printf "\e[0m\e[1A\e[7D %s" "$spin"
+      sleep "$delay"
+    done
+  done
+  printf "\e[0m\e[1A\e[7D\n"
+}
+
+# Stop ceremonyclient service (with spinner)
+service ceremonyclient stop &
+spin_pid=$!
+echo -n "Stopping ceremonyclient service..."
+spinner $spin_pid
+wait $spin_pid
+
+# Navigate to ceremonyclient directory
 cd ~/ceremonyclient
-git fetch origin
-git merge origin
+
+# Fetch updates (with spinner)
+git fetch origin &
+spin_pid=$!
+echo -n "Fetching latest updates..."
+spinner $spin_pid
+wait $spin_pid
+
+# Merge updates (with spinner)
+git merge origin &
+spin_pid=$!
+echo -n "Merging updates..."
+spinner $spin_pid
+wait $spin_pid
+
+# Move to node directory
 cd ~/ceremonyclient/node
-GOEXPERIMENT=arenas go clean -v -n -a ./...
+
+# Clean project (with spinner)
+GOEXPERIMENT=arenas go clean -v -n -a ./... &
+spin_pid=$!
+echo -n "Cleaning project..."
+spinner $spin_pid
+wait $spin_pid
+
+# Remove old binary
 rm /root/go/bin/node
+
+# List contents of /root/go/bin (optional)
 ls /root/go/bin
-GOEXPERIMENT=arenas  go  install  ./...
+
+# Build and install project (with spinner)
+GOEXPERIMENT=arenas go install ./... &
+spin_pid=$!
+echo -n "Building and installing..."
+spinner $spin_pid
+wait $spin_pid
+
+# List contents of /root/go/bin (optional)
 ls /root/go/bin
+
+# Start ceremonyclient service
 service ceremonyclient start
+
+# Clear terminal screen
 clear
+
+# Follow ceremonyclient service logs
 sudo journalctl -u ceremonyclient.service -f --no-hostname -o cat
